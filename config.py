10ebc7041cafaac8f83888a9145f7cefa992fa4a
MaysaB <49612167+MaysaB@users.noreply.github.com>
2022-09-08 16:12:18 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+API_KEY = '********'
+
+API_SECRET = '********'
+
+ACCESS_TOKEN = '********'
+
+ACCESS_TOKEN_SECRET = '********'
+
+BEARER_TOKEN = '********'
+
+CLIENT_ID = '********'
+
+CLIENT_SECRET = '********'
